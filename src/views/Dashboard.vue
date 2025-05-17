0b448887b3c2449ff82df272636adcb1cc33a5b0
Ithalo <ithalozimermann96@gmail.com>
2025-05-16 21:45:49 -0300

    Dashboard Inicio

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,13 +1,33 @@
 <script setup>
 import Header from './components/Header.vue'
+import CardBox from './components/CardBox.vue'
 </script>
 
 <template>
     <div class="">
-        <Header>
-        </Header>
-        <div class="">
-            
+        <Header></Header>
+        <div class="d-flex flex-wrap align-items-start justify-content-start gap-5 mt-5 ms-5">
+            <CardBox 
+            icon='<i class="fa-light fa-user-tie"></i>' 
+            title="Professores" 
+            subtitle="R$ 56,50" 
+            color="green" />
+            <CardBox 
+                icon='<i class="fa-light fa-users-line"></i>' 
+                title="Alunos" 
+                subtitle="20" 
+                color="Orange"/>
+            <CardBox 
+                icon='<i class="fa-light fa-screen-users"></i>' 
+                title="Turmas" 
+                subtitle="20" 
+                color="Red"/>  
+            <CardBox 
+                icon='<i class="fa-light fa-court-sport"></i>' 
+                title="Modalidades" 
+                subtitle="teste" 
+                color="Blue"/>  
         </div>
     </div>
 </template>
+
---
+++ src/views/components/CardBox.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="card-box shadow-sm rounded d-flex justify-content-between p-3 bg-white">
+      <div>
+        <h4 class="fw-semibold text-muted ">{{ title }}</h4>
+        <p class="fw-bold mb-0">{{ subtitle }} <span class="fs-6 text-primary ms-2">Ver todos</span></p>
+      </div>
+      <div class="rounded icon-wrapper" :style="'background-color:'+color">
+        <div v-html="icon"></div>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  defineProps(['icon', 'title', 'subtitle', 'color'])
+  </script>
+  
+  <style>
+  .card-box {
+    min-width: 300px;
+    height: 160px;
+    box-shadow: 0 0 8px 0px #0000000d;
+    transition: box-shadow 0.2s;
+    display: flex;
+  }
+  
+  .card-box:hover {
+    box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
+  }
+  
+  .icon-wrapper {
+    width: 50px;
+    height: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 22px;
+    color: white;
+  }
+
+  </style>
+  
