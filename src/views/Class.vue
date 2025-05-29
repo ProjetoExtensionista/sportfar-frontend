2c2f42be0ebda80d6f1f1d0b3d1f23349d75e445
MatheusPnssn <matheuspnssn@gmail.com>
2025-05-29 19:14:33 -0300

    ajustes class page

--- src/views/Class.vue
+++ src/views/Class.vue
@@ -16,11 +16,11 @@ import Header from './components/Header.vue'
                         <p class="mb-0">Formulário para criação de turmas</p>
                     </div>
                 </div>
-                <div class="d-flex submain-block h-100 w-100">
+                <div class="d-flex submain-block  w-100 p-5 overflow-auto">
                     <div class="col-6 p-7 form">
                         <div class="mb-3 d-flex justify-content-center">
                             <label for="modality" class="col-sm-2 col-form-label">Modalidade</label>
-                            <select id="modality" class="form-select" >
+                            <select name="modality" class="form-select" >
                                 <option selected>Open this select menu</option>
                                 <option value="1">One</option>
                                 <option value="2">Two</option>
@@ -30,49 +30,51 @@ import Header from './components/Header.vue'
                         <div class="mb-3 d-flex justify-content-center">
                             <label for="date" class="col-sm-2 col-form-label">Data de Realização</label>
                             <div class="double-input d-flex">
-                                <input type="date" class="form-control" id="start_date">
-                                <input type="date" class="form-control" id="final_date"> 
+                                <input type="date" class="form-control" name="start_date">
+                                <input type="date" class="form-control" name="final_date"> 
                             </div>
                         </div>
                         <div class="mb-3 d-flex justify-content-center">
                             <label for="workload" class="col-sm-2 col-form-label">Carga Horária</label>
-                            <input type="text" class="form-control" id="workload" v-model="workload" @input="filterNumbers" placeholder="4">
+                            <input type="text" class="form-control" name="workload" v-model="workload" @input="filterNumbers('workload')" placeholder="4">
+                        </div>
+                        <div class="mb-3 d-flex justify-content-center">
+                            <label for="teacher" class="col-sm-2 col-form-label">Professor</label>
+                            <input type="text" class="form-control" name="teacher" placeholder="Selecione o(s) professor(es)">
+                        </div>
+                        <div class="mb-3 d-flex justify-content-center">
+                            <label for="customRange1" class="form-label">Quant. Vagas</label>
+                            <input v-model="vacant"  @input="printVacant" type="range" step="5" min="0" max="100" value="10" class="form-range" name="vacant">
+                        </div>
+                        <div class="mb-3 d-flex justify-content-center">
+                            <label for="teacher" class="col-sm-2 col-form-label">Status da Turma</label>
+                            <div class="d-flex justify-content-start gap-2 form-block-input">
+                                <button type="button" class="btn btn-primary">Aberta</button>
+                                <button type="button" class="btn btn-secondary">Fechada</button>
+                                <button type="button" class="btn btn-secondary">Inativa</button>
+                            </div>
                         </div>
                         <div class="mb-3 d-flex justify-content-center">
                             <label for="inputPassword" class="col-sm-2 col-form-label">Dia da Semana</label>
-                            <select class="form-select" aria-label="Default select example">
-                                <option selected>Open this select menu</option>
+                            <select class="form-select" name="weekday">
+                                <option selected>Selecione o dia da semana</option>
                                 <option value="1">One</option>
                                 <option value="2">Two</option>
                                 <option value="3">Three</option>
                             </select>
                         </div>
-                         <div class="mb-3 d-flex justify-content-center">
-                            <label for="teacher" class="col-sm-2 col-form-label">Professor</label>
-                            <input type="text" class="form-control" id="teacher" placeholder="Selecione o(s) professor(es)">
-                        </div>
                         <div class="mb-3 d-flex justify-content-center">
-                            <label for="customRange1" class="form-label">Example range</label>
-                            <input type="range" step="10" class="form-range" id="customRange1" list="tickmarks">
-                            <datalist id="tickmarks">
-                                <option value="0">0</option>
-                                <option value="10">2</option>
-                                <option value="20"></option>
-                                <option value="30"></option>
-                                <option value="40"></option>
-                                <option value="50"></option>
-                                <option value="60"></option>
-                                <option value="70"></option>
-                                <option value="80"></option>
-                                <option value="90"></option>
-                                <option value="100"></option>
-                            </datalist>
-                        </div>
-                        <div class="mb-3 d-flex justify-content-center">
-                            <label for="inputPassword" class="col-sm-2 col-form-label">Carga Horária</label>
+                            <label for="inputPassword" class="col-sm-2 col-form-label">Horário da Aula</label>
                             <div class="double-input d-flex">
-                                <input type="time" class="form-control" id="inputPassword">
-                                <input type="time" class="form-control" id="inputPassword"> 
+                                <input type="time" class="form-control" name="start_time">
+                                <input type="time" class="form-control" name="end_time"> 
+                            </div>
+                        </div>
+                        <div class="mb-3 d-flex justify-content-center">
+                            <label for="inputPassword" class="col-sm-2 col-form-label">Faixa Etária</label>
+                            <div class="double-input d-flex">
+                                <input type="text" class="form-control" name="age_group_1" v-model="age_group_1" @input="filterNumbers('age_group_1')">
+                                <input type="text" class="form-control" name="age_group_2" v-model="age_group_2" @input="filterNumbers('age_group_2')">  
                             </div>
                         </div>
                     </div>
@@ -87,16 +89,18 @@ import Header from './components/Header.vue'
 
 <script>
 export default {
-  data() {
-    return {
-      workload: ''
-    };
-  },
-  methods: {
-    filterNumbers() {
-      this.workload = this.workload.replace(/\D/g, '');
+    data() {
+        return {
+            workload: '',
+            age_group_1: '',
+            age_group_2: ''
+        };
+    },
+    methods: {
+        filterNumbers(field) {
+            this[field] = this[field].replace(/\D/g, '');
+        }
     }
-  }
 };
 </script>
 
@@ -138,15 +142,19 @@ body, p, h1, h2, h3, h4, h5, h6, label, span {
 }
 
 #class .submain-block {
+    height: calc(100% - 93px);
     border-top: 3px solid #d9d9d9;
     border-left: 3px solid #d9d9d9;
+}
+#class .submain-block .form {
+    border-right: 3px solid #d9d9d9;
 }
 
 #class .submain-block .form label {
     min-width: 170px;
 }
 
-#class .submain-block .form select, #class .submain-block .form input, #class .submain-block .form .double-input{
+#class .submain-block .form select, #class .submain-block .form .form-block-input, #class .submain-block .form input, #class .submain-block .form .double-input{
     width: 100%;
     max-width: 360px;
     margin: 0;
@@ -167,6 +175,29 @@ body, p, h1, h2, h3, h4, h5, h6, label, span {
 #class .submain-block .form .double-input input:first-child{
     border-top-right-radius:0;
     border-bottom-right-radius:0;
+}
+
+#class .submain-block .form .tickmarks-5 span{
+    width: 0px;
+    position: absolute;
+    font-size: 12px;
+    line-height: 70%;
+}
+
+#class .submain-block .form .tickmarks-5 span:last-child{
+    left: 5.5px;
+}
+
+#class .submain-block .form .tickmarks-5 span::before {
+  content: '';
+  position: absolute;
+  width: 11px;
+  height: 11px;
+  background-color: white;
+  border: 1px solid #0d6efd;
+  border-radius: 50%;
+  top: -23px;
+  left: calc(50% - 5.5px);
 }
 
 #class .sub-header {
