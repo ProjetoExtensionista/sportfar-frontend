1c481237dddd0acba850072bb2b83796c34cf408
MatheusPnssn <matheuspnssn@gmail.com>
2025-06-06 20:16:01 -0300

    ajuste login e session

--- ConfigAxios.js
+++ ConfigAxios.js
@@ -1,11 +1,23 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://projetos.farroupilha.ifrs.edu.br:35010',
-    timeout: 1000,
-    headers: {
-        "accessToken": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluQGFkbWluLmNvbSIsInN1YiI6MCwiaWF0IjoxNzQ3NDQwODE0LCJleHAiOjE3NDc0NDQ0MTR9.X1gieH4J8CW0Ei8QtFkvYFfk7Pt3Ktcym2IURgttg78"
-    }
+  baseURL: 'http://projetos.farroupilha.ifrs.edu.br:35010',
+  timeout: 1000
 });
 
-export {instance}
+instance.interceptors.request.use(config => {
+  const token = sessionStorage.getItem('accesstoken');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, error => {
+  return Promise.reject(error);
+});
+
+const instanceAuth = axios.create({
+  baseURL: 'http://projetos.farroupilha.ifrs.edu.br:35010',
+  timeout: 1000
+});
+
+export { instance, instanceAuth };
