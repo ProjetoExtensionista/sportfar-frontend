5ccfe1140139a029136d5df77a7d91509faf25b6
Pamela Maria da Silva <pamela@testbox.com>
2025-07-04 18:57:04 -0300

    feat: envio de presenças

--- src/services/classService.js
+++ src/services/classService.js
@@ -1,11 +1,13 @@
-import api from './api';
-import handleResponse from '../utils/handleResponse';
+import api from "./api";
+import handleResponse from "../utils/handleResponse";
 
 export default {
-    getByClassId(id) {
-        return handleResponse(api.get(`/classRoom/getClassId/${id}`));
-    },
-    getAbsencesByClassRoomId(id) {
-        return handleResponse(api.get(`/classRoom/getStudentsAbsenceByClassId/${id}`));
-    }
+  getByClassId(id) {
+    return handleResponse(api.get(`/classRoom/getClassId/${id}`));
+  },
+  getAbsencesByClassRoomId(id) {
+    return handleResponse(
+      api.get(`/classRoom/getStudentsAbsenceByClassRoomId/${id}`)
+    );
+  },
 };
